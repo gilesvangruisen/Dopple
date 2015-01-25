9d6488553e8930967d676e4a401c858b5b72220e
Giles Van Gruisen <gilesvangruisen@me.com>
2015-01-25 16:24:16 -0500

    Document and cleanup BoardsViewController(s)

--- Dopple/BoardViewController.swift
+++ Dopple/BoardViewController.swift
@@ -7,15 +7,8 @@
 //
 
 import UIKit
-
 import AVFoundation
-import Pipeline
 import Publinks
-
-enum BoardState: Int {
-    case Record = 0
-    case Playback = 1
-}
 
 class BoardViewController: UIViewController {
 
@@ -23,29 +16,32 @@ class BoardViewController: UIViewController {
     @IBOutlet var middleButton: Button!
 
     override func viewDidLoad() {
+
+        // Start audio session for play and record, use real speaker
         var error: NSError?
         AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord, error: &error)
         AVAudioSession.sharedInstance().setActive(true, error: &error)
         AVAudioSession.sharedInstance().overrideOutputAudioPort(AVAudioSessionPortOverride.Speaker, error: &error)
 
+        // Subscribe to middleButton's up/downlink
         middleButton.downlink >>> middleDown
         middleButton.uplink >>> middleUp
     }
 
     func middleDown(button: Button) {}
-
     func middleUp(button: Button) {}
 
 }
 
 class PlaybackViewController: BoardViewController {
 
+    /** Player objects built automatically when `urls` is set */
     var players: [Player] = [Player]()
 
-    let playbackQueue = dispatch_queue_create("com.gilesvangruisen.Dopple.playback", DISPATCH_QUEUE_SERIAL)
-
+    /** URLs of audio files to be played */
     var urls: [NSURL]! {
         didSet {
+            // Build players
             players = urls.map { Player(sound: $0) }
         }
     }
@@ -53,62 +49,85 @@ class PlaybackViewController: BoardViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        urls = recordingURLs(8)
+        // Set URLs
+        urls = recordingURLs(8, "m4a")
 
+        // Enumerate buttons and subscribe to each
         for (index, button) in enumerate(buttons) {
 
-            button.downlink >>> { button in
-                dispatch_async(self.playbackQueue) {
+            // Subscribe to each button's downlink
+            button.downlink >>> { _ in
 
-                    self.players = self.players.filter { player in
-                        return player.audioPlayer?.url! != self.urls[index]
-                    }
-
-                    let player = Player(sound: self.urls[index])
-                    self.players += [player]
-
-                    player.playFromBeginning()
-                }
+                // Call playSound with button index
+                self.playSound(index)
             }
         }
     }
 
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    /** Player sound from URL at specified index */
+    func playSound(index: Int) {
 
+        // Remove existing player for same sound
+        self.players = self.players.filter { player in
+            return player.audioPlayer?.url! != self.urls[index]
+        }
+
+        // Build new player and add to players
+        let player = Player(sound: self.urls[index])
+        self.players += [player]
+
+        // Play sound
+        player.playFromBeginning()
+    }
+
+
+    // MARK: Navigation
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Set urls upon transition
         let recordVC = segue.destinationViewController as RecordViewController
         recordVC.urls = urls
-
     }
 
 }
 
 class RecordViewController: BoardViewController {
 
+    /** Recorder used to record, reset upon each recording */
     var recorder: Recorder!
 
+    /** Audio destination file URLs */
     var urls: [NSURL]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Enumerate buttons and subscribe to each
         for (index, button) in enumerate(buttons) {
 
+            // Start recording for index upon button down
             button.downlink >>> { _ in
-                self.recorder = Recorder(URL: self.urls[index])
-                self.startRecording()
+                self.startRecording(index)
             }
 
+            // Stop recording upon button up
             button.uplink >>> stopRecording
         }
     }
 
-    func startRecording() {
+    /** Start recording to URL at specified index */
+    func startRecording(index: Int) {
+        recorder = Recorder(URL: self.urls[index])
         recorder.record()
     }
 
+    /** Stop recording and write to URL at index specified upon `startRecording(index: Int)` */
     func stopRecording(button: Button) {
         recorder.stop()
     }
+
+
+    // MARK: BoardViewController override
 
     override func middleUp(button: Button) {
         let playbackVC = self.presentingViewController as PlaybackViewController
@@ -119,17 +138,16 @@ class RecordViewController: BoardViewController {
 
 }
 
-public func recordingURLs(n: Int) -> [NSURL] {
-    let paths: NSArray = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
-    var urls = [NSURL]()
+// Returns array of `n` recording URLs w/ specified `ext` in docs dir. (e.g. .../Documents/recording0.m4a)
+public func recordingURLs(n: Int, ext: String) -> [NSURL] {
 
-    for num in 0...n - 1 {
-        let outputFileBase = NSURL.fileURLWithPathComponents([paths.lastObject as String, "recording\(num).m4a"])
-        println("URL: \(outputFileBase)")
-        if let url = outputFileBase {
-            urls += [url]
-        }
+    // Find basePath in documents directory
+    let basePath = (NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true) as NSArray).lastObject! as String
+
+    // Return 0..<n mapped to [NSURL] where each = basePath + "recording\(i).\(ext)"
+    return Array(0..<n).map { i -> NSURL in
+        return NSURL.fileURLWithPathComponents([basePath, "recording\(i).\(ext)"])!
     }
 
-    return urls
 }
+
